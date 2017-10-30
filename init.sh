290ab3a45f987e2ccff025c919bb4ca609cdbd7e
Sebastian Krzyszkowiak <dos@dosowisko.net>
2017-10-30 21:09:22 +0100

    add initial, crude version of init script

---
+++ init.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+# This script can be used standalone. It creates project named $1 in the current directory.
+set -e
+
+git clone https://github.com/dos1/libsuperderpy-examples.git $1
+cd $1
+rm -rf .git
+git init
+git config -f .gitmodules --get-regexp '^submodule\..*\.path$' |
+    while read path_key path
+    do
+        url_key=$(echo $path_key | sed 's/\.path/.url/')
+        url=$(git config -f .gitmodules --get "$url_key")
+        rmdir $path
+        git submodule add $url $path
+    done
+# TODO: icons, desktop file, gamenames in cmake
+git add .
+git commit -am "initial commit"

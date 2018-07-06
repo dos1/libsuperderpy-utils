c6c827d8140820d28cdd93488e1c0be4f5e34167
Sebastian Krzyszkowiak <dos@dosowisko.net>
2018-07-06 22:08:22 +0200

    add script for webassembly builds

--- package_all.sh
+++ package_all.sh
@@ -1,9 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 
 ./package_linux.sh
 ./package_win32.sh
 ./package_win64.sh
 ./package_osx.sh
-./package_android.sh
 ./package_html5.sh
+./package_wasm.sh
+./package_android.sh
---
+++ package_wasm.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -e
+
+mkdir -p output
+
+rm -rf build-wasm
+
+. /usr/lib/emsdk/emsdk_env.sh /usr/lib/emsdk
+
+pushd .
+
+mkdir build-wasm
+
+cd build-wasm
+
+emcmake cmake ../.. -DCMAKE_BUILD_TYPE=RelWithDebInfo -DALLEGRO_INCLUDE_PATH=$ALLEGRO_EMSCRIPTEN_DIR/include -DALLEGRO_LIBRARY_PATH=$ALLEGRO_EMSCRIPTEN_DIR/lib/liballegro_monolith-static.a -DCMAKE_INSTALL_PREFIX=output -DUSE_CLANG_TIDY=no -DLIBSUPERDERPY_EMSCRIPTEN_MODE=wasm -G Ninja
+
+GAMENAME=`grep LIBSUPERDERPY_GAMENAME:INTERNAL CMakeCache.txt`
+GAMENAME=${GAMENAME#LIBSUPERDERPY_GAMENAME:INTERNAL=}
+
+GAMENAME_PRETTY=`grep LIBSUPERDERPY_GAMENAME_PRETTY:INTERNAL CMakeCache.txt`
+GAMENAME_PRETTY=${GAMENAME_PRETTY#LIBSUPERDERPY_GAMENAME_PRETTY:INTERNAL=}
+
+ninja
+ninja ${GAMENAME}_js
+
+cd output/$GAMENAME
+zip -9r $GAMENAME-wasm.zip *
+mv $GAMENAME-wasm.zip ../../../output/
+
+popd
+
+rm -rf build-wasm

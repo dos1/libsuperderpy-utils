8e3c102677d10bbbbd6151d0b3128cf53709b64c
Sebastian Krzyszkowiak <dos@dosowisko.net>
2019-02-06 01:58:29 +0100

    update maemo libs

--- libs/update-maemo5.sh
+++ libs/update-maemo5.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-docker run --rm -it --privileged -v /home/dos/git/boiledcorn/utils/libs/maemo5:/libs --entrypoint /bin/bash dosowisko/libsuperderpy-maemo5 -c "cp /scratchbox/users/admin/targets/FREMANTLE_ARMEL/usr/local/lib/* /libs/"
+docker run --rm -it --privileged -v `realpath maemo5`:/libs --entrypoint /bin/bash dosowisko/libsuperderpy-maemo5 -c "cp /scratchbox/users/admin/targets/FREMANTLE_ARMEL/usr/local/lib/* /libs/"
+rm maemo5/*.a

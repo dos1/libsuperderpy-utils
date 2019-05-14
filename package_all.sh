25d1b79c6b1cb603162c20e0e2cb74e23d2e9780
Sebastian Krzyszkowiak <dos@dosowisko.net>
2019-05-14 07:51:26 +0200

    add support for pocketchip builds

--- package_all.sh
+++ package_all.sh
@@ -12,4 +12,5 @@ podman run --rm -it -v `realpath ..`:/src -w /src/utils dosowisko/libsuperderpy-
 podman run --rm -it -v `realpath ..`:/src -w /src/utils dosowisko/libsuperderpy-emscripten /src/utils/package_wasm.sh
 podman run --rm -it -v `realpath ..`:/src -w /src/utils dosowisko/libsuperderpy-emscripten /src/utils/package_html5.sh
 podman run --rm -it -v `realpath ..`:/src -w /src/utils dosowisko/libsuperderpy-steamlink /src/utils/package_steamlink.sh
+podman run --rm -it -v `realpath ..`:/src -w /src/utils dosowisko/libsuperderpy-pocketchip /src/utils/package_pocketchip.sh
 podman run --rm -it -v `realpath ..`:/src -w /src/utils -e ANDROID_KEYSTORE_PASSWORD=$LIBSUPERDERPY_ANDROID_KEYSTORE_PASSWORD -e ANDROID_KEYSTORE_BASE64="`base64 -w0 $LIBSUPERDERPY_ANDROID_KEYSTORE`" dosowisko/libsuperderpy-android-armv7 /src/utils/release_android.sh git

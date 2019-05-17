3a609b7844e22c8cf643c8e929089e19b8d61062
Sebastian Krzyszkowiak <dos@dosowisko.net>
2019-05-17 17:23:10 +0200

    package_android_armv7.sh: support keystore provided in base64

--- package_android_armv7.sh
+++ package_android_armv7.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 test -t 1 && USE_TTY="-t"
 mkdir -p ../.assetcache
-podman run --rm -i $USE_TTY -v `realpath ..`:/src -w /src/utils -e ANDROID_KEYSTORE_PASSWORD=$LIBSUPERDERPY_ANDROID_KEYSTORE_PASSWORD -e ANDROID_KEYSTORE_BASE64="`base64 -w0 $LIBSUPERDERPY_ANDROID_KEYSTORE`" dosowisko/libsuperderpy-android-armv7 /src/utils/build-scripts/build_android_armv7.sh $@
+
+if [ "$LIBSUPERDERPY_ANDROID_KEYSTORE" ]; then
+  LIBSUPERDERPY_ANDROID_KEYSTORE_BASE64="`base64 -w0 $LIBSUPERDERPY_ANDROID_KEYSTORE`"
+fi
+
+podman run --rm -i $USE_TTY -v `realpath ..`:/src -w /src/utils -e ANDROID_KEYSTORE_PASSWORD="$LIBSUPERDERPY_ANDROID_KEYSTORE_PASSWORD" -e ANDROID_KEYSTORE_BASE64="$LIBSUPERDERPY_ANDROID_KEYSTORE_BASE64" dosowisko/libsuperderpy-android-armv7 /src/utils/build-scripts/build_android_armv7.sh $@

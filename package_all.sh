daa6018ecc28cca84ffb428b0b28fc5ed0766098
Sebastian Krzyszkowiak <dos@dosowisko.net>
2019-05-17 23:24:05 +0200

    update package_all.sh

--- package_all.sh
+++ package_all.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 set -e
 
+if [ "$(grep SDL ../flatpak/*.json)" ]; then
+  FLATPAK_SDL=1
+fi
+
+echo $FLATPAK_SDL
+
 ./package_maemo5.sh
 ./package_linux_flatpak_arm64.sh
-#./package_linux_flatpak_armv7.sh # requires SDL backend in flatpak manifest
+[ "$FLATPAK_SDL" ] && ./package_linux_flatpak_armv7.sh
 ./package_linux_flatpak_amd64.sh
-#./package_linux_flatpak_i686.sh # requires SDL backend in flatpak manifest
+[ "$FLATPAK_SDL" ] && ./package_linux_flatpak_i686.sh
 ./package_linux_amd64.sh
 ./package_linux_i686.sh
 ./package_win64.sh
@@ -18,3 +24,4 @@ set -e
 ./package_pocketchip.sh
 ./package_switch.sh
 ./package_android_armv7.sh git
+./package_android_arm64.sh git

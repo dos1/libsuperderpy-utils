889ca1edf8e57b61b46bf95bd4830456b4488abb
Sebastian Krzyszkowiak <dos@dosowisko.net>
2019-05-19 02:59:31 +0200

    read_cmake_var.sh: use /bin/sh as a shebang

--- build-scripts/read_cmake_var.sh
+++ build-scripts/read_cmake_var.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 LINE=`egrep -i '[^ \t]*set[ \t]*\([ \t]*'"$1"'(.*)\)' ../CMakeLists.txt`
 echo $LINE | sed -ne 's/^\s*set\s*(\s*\(.*\)\s*)\s*$/\1/pi' | awk -F'"' '{ print $2 }'

ce98d8fdb32bb85952e2a24810675e013603f7a8
Sebastian Krzyszkowiak <dos@dosowisko.net>
2019-05-29 18:10:06 +0200

    init.sh: use shallow clones where sensible and make sure all nested submodules are checked out

--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@
 # This script can be used standalone. It creates project named $1 in the current directory.
 set -e
 
-git clone https://gitlab.com/dosowisko.net/libsuperderpy-examples.git $1
+git clone --recurse-submodules=false --depth=1 https://gitlab.com/dosowisko.net/libsuperderpy-examples.git $1
 cd $1
 rm -rf .git
 git init
@@ -15,6 +15,7 @@ git config -f .gitmodules --get-regexp '^submodule\..*\.path$' |
         repo=$(echo $url | sed -e 's/^\.\.\/\.\.\///g')
         git submodule add https://gitlab.com/$repo $path
     done
+git submodule update --init --recursive --depth=1
 # TODO: icons, desktop file, gamenames in cmake
 sed -i -e 's/https:\/\/gitlab\.com\//..\/..\//' .gitmodules
 git add .
